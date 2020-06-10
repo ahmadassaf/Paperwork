ec10f02cb0120539da63df09e2f347983a781445
マリウス <marius@twostairs.co>
2020-06-10 23:28:07 +0100

    Updated PeeringService

--- paperwork/src/lib/peering.ts
+++ paperwork/src/lib/peering.ts
@@ -1,49 +1,67 @@
 import Peer, { DataConnection } from 'peerjs';
 
-export class PeeringService {
-  peer: Peer;
-  id: string;
-  connections: Array<DataConnection>;
+type TPeeringServiceConfig = {
+  peerServer?: {
+    host: string;
+    key: string;
+    port: number;
+    path: string;
+  };
 
-  constructor() {
-    this.peer = new Peer(undefined, {
+  handlers?: {
+    onOpen?: Function;
+    onConnection?: Function;
+    onDisconnected?: Function;
+    onClose?: Function;
+    onError?: Function;
+  };
+};
+
+export class PeeringService {
+  _config: TPeeringServiceConfig;
+  _peer: Peer;
+  _id: string;
+  _connections: Array<DataConnection>;
+
+  constructor(config: TPeeringServiceConfig) {
+    this._peer = new Peer(undefined, {
       'host': '192.168.8.112',
       'key': 'peerjs',
       'port': 9000,
       'path': '/peerjs'
     });
-    this.id = '';
-    this.connections = [];
+    this._id = '';
+    this._connections = [];
 
-    this.peer.on('open', (id) => {
-      this.id = id;
-      console.log(`My ID: ${this.id}`);
+    this._peer.on('open', (id) => {
+      this._id = id;
+      console.log(`My ID: ${this._id}`);
     });
 
-    this.peer.on('connection', (conn: DataConnection) => {
+    this._peer.on('connection', (conn: DataConnection) => {
       console.log(`New connection:`);
       console.log(conn);
 
-      this.handle(conn);
+      this._handle(conn);
 
-      this.connections.push(conn);
+      this._connections.push(conn);
     });
 
-    this.peer.on('disconnected', () => {
+    this._peer.on('disconnected', () => {
       console.log(`Disconnetced! Reconnecting ...`);
-      this.peer.reconnect();
+      this._peer.reconnect();
     });
 
-    this.peer.on('close', () => {
+    this._peer.on('close', () => {
       console.log(`Closed!`);
     });
 
-    this.peer.on('error', (err) => {
+    this._peer.on('error', (err) => {
       console.error(err);
     });
   }
 
-  handle(conn: DataConnection): boolean {
+  _handle(conn: DataConnection): boolean {
     conn.on('data', (data: string) => {
       console.log(`Data:`);
       console.log(data);
@@ -52,10 +70,10 @@ export class PeeringService {
     conn.on('close', () => {
       console.log(`Closed connection!`);
 
-      const connIdx: number = this.connections.findIndex(connection => connection == conn);
+      const connIdx: number = this._connections.findIndex(connection => connection === conn);
 
       if(connIdx > 0) {
-        this.connections.splice(connIdx, 1);
+        this._connections.splice(connIdx, 1);
       }
     });
 
@@ -64,25 +82,25 @@ export class PeeringService {
 
   async connect(id: string): Promise<number> {
     return new Promise((fulfill, reject) => {
-      const conn = this.peer.connect(id, { 'reliable': true });
+      const conn = this._peer.connect(id, { 'reliable': true });
 
       conn.on('error', (err) => {
         console.error(err);
         return reject(err);
       });
 
-      this.handle(conn);
+      this._handle(conn);
 
       conn.on('open', () => {
         console.log(`Connected:`);
         console.log(conn.peer);
-        return fulfill((this.connections.push(conn) - 1));
+        return fulfill((this._connections.push(conn) - 1));
       });
     });
   }
 
   send(connIdx: number, data: string) {
-    return this.connections[connIdx].send(data);
+    return this._connections[connIdx].send(data);
   }
 
 }
