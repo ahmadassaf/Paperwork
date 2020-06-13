16eff453d0fdc8f4c814a14dcf5fdd0f93965261
マリウス <marius@twostairs.co>
2020-06-13 16:52:40 +0100

    Included Supervisor for testing

--- paperwork/src/App.tsx
+++ paperwork/src/App.tsx
@@ -24,6 +24,8 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import Supervisor from './lib/Supervisor';
+Supervisor.launch();
 
 // const notesService = new NotesService();
 // const bla = async() => {
